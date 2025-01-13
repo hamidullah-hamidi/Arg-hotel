e095e01a0a7d401a786580aa63260b8803325446
Hamidullah <hamidullahh754@gmail.com>
2025-01-14 00:30:46 +0430

    completed the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 function App() {
   return (
     <div className='relative h-screen w-full'>
       <div className='background'></div>
-      <div className='relative text-white'>
-        <div className='h-[470px] w-[] bg-red-900 rounded-b-[50%] shadow-2xl shadow-black flex justify-center items-center flex-col gap-10 '>
-          <h1 className='text-4xl '>ARG HOTEL</h1>
-          <div className='relative w-full h-screen flex justify-center items-center overflow-hidden'>
-            <div className='slide fadeIn delay-700'>
-              <img
-                src='./images/3.png'
-                alt='Hotel'
-                className='object-cover w-full h-full'
-              />
+      <div className='relative text-white text-center'>
+        <div className='h-[470px] w-[] bg-red-900 rounded-b-[50%] shadow-2xl shadow-black flex justify-center items-center flex-col  '>
+          <h1 className='text-4xl mt-20'>ARG HOTEL</h1>
+          <div className='container relative w-full h-screen flex justify-center items-center overflow-hidden'>
+            <div className='smoke'>
+              <img src='.' alt='' />
             </div>
-            <div className='slide fadeIn delay-[5s]'>
-              <img
-                src='./images/1.png'
-                alt='Hotel'
-                className='object-cover w-full h-full'
-              />
+            <div className='image-box'>
+              <img src='./images/1.png' alt='Image 1' />
             </div>
-            <div className='slide fadeIn delay-[7s]'>
-              <img
-                src='./images/2.png'
-                alt='Hotel'
-                className='object-cover w-full h-full'
-              />
+            <div className='image-box'>
+              <img src='./images/2.png' alt='Image 2' />
+            </div>
+            <div className='image-box'>
+              <img src='./images/3.png' alt='Image 3' />
             </div>
           </div>
         </div>
-        <div>
-          <h1 className='text-2xl'>خوش آمیدید</h1>
-          <p className='text-lg'>از اینکه هوتل ارگ انتخاب نمودید از شما سپاسگذاریم</p>
-          <button className='mt-4 py-2 px-4 bg-blue-500 text-white rounded'>منو</button>
+        <div className=' mt-10 px-36 flex justify-center items-center flex-col animate-fadeIn'>
+          <h1 className='text-3xl font-semibold text-orange-500'>خوش آمدید</h1>
+          <p className='my-2'>از اینکه هوتل ارگ انتخاب نمودید از شما سپاسگذاریم</p>
+          <span className='mt-3 pr-5  border h-11 text-white text-3xl rounded-full flex justify-center items-center '>
+            <i className='material-icons text-5xl left-right'>chevron_left</i>
+            منو
+          </span>
         </div>
-        <footer className='mt-6'>
-          <h1 className='text-xl'>ARG HOTEL (آیکن ها)</h1>
-          <p className='text-lg'>www.arghotel.af</p>
+        <footer className='mt-8 flex flex-col justify-center items-center'>
+          <span className='flex gap-1 '>
+            ARG Hotel
+            <i className='fa-solid fa-chevron-left mx-2'></i>
+            <i className='fa-brands fa-instagram'></i>
+            <i className='fa-brands fa-facebook'></i>
+            <i className='fa-brands fa-telegram'></i>
+            <i className='fa-brands fa-whatsapp'></i>
+          </span>
+          <p className=' tracking-widest text-sm'>www.arghotel.af</p>
         </footer>
       </div>
     </div>

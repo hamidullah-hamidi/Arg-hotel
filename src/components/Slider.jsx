6f1d646b9f26b9df888098430a8a612186656374
Hamidullah <hamidullahh754@gmail.com>
2025-01-17 17:19:20 +0430

    added the slider part of menu

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,10 +3,10 @@ import '../App.css';
 function Slider() {
   return (
     <div className='slider h-[40%] px-4 pt-4'>
-      <header className='flex justify-between mx-4 items-center'>
+      <header className='flex justify-between m-4 items-center'>
         <div>
           <h1 className=' text-2xl'>لذت های طعم دار</h1>
-          <p className=' text-gray-500 tracking-widest'>انتخاب کنید و امتحان کنید</p>
+          <p className=' text-stone-400 tracking-wider'>انتخاب کنید و امتحان کنید</p>
         </div>
         <div className=' w-[90px]'>
           <img src='/images/logo.png' alt='' />
@@ -14,10 +14,10 @@ function Slider() {
       </header>
       <main className=' bg-gray-500 rounded-2xl bg-opacity-60 h-[140px] flex justify-between items-center px-4'>
         <div className=' h-full py-4'>
-          <p className=' mb-6 text-gray-300 '>پیشنهاد ارگ</p>
-          <h1 className=' text-2xl'>میکس پلیتر</h1>
-          <div className='flex gap-8'>
-            <p>مخصوص</p>
+          <p className=' mb-6 text-gray-300 text-lg '>پیشنهاد ارگ</p>
+          <h1 className=' text-2xl font-extrabold '>میکس پلیتر</h1>
+          <div className='flex gap-5 text-sm'>
+            <p >مخصوص</p>
             <p>زمان: 30 دقیقه</p>
           </div>
         </div>

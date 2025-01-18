9450fc000fd0f0eb7c78e5a1131a2f82a8bc3760
Hamidullah <hamidullahh754@gmail.com>
2025-01-18 17:02:50 +0430

    u

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,35 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "../App.css";
+import { useEffect, useState } from "react";
+import { slides } from "../data/slide";
 
 function Slider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // تغییر اسلاید به جلو
+  const nextSlide = () => {
+    if (currentSlide < slides.length - 1) {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    } else setCurrentSlide(0);
+  };
+
+  // تغییر اسلاید به عقب
+  const prevSlide = () => {
+    if (currentSlide) {
+      setCurrentSlide((prev) => (prev - 1) % slides.length);
+    }
+  };
+
+  useEffect(() => {
+    function changeSlide() {
+      setInterval(nextSlide, 1000);
+    }
+    return () => clearInterval(changeSlide);
+  }, []);
+
+  const currentItem = slides[currentSlide];
+
   return (
     <div className="slider  w-full px-4 py-4 md:pt-0">
-      <header className="flex justify-between m-4 items-center">
+      <header className="flex justify-between m-4  items-center">
         <div className=" w-[90px] -mt-3">
           <img src="/images/logo.png" alt="" />
         </div>
@@ -15,20 +40,41 @@ function Slider() {
           </p>
         </div>
       </header>
-      
-      {/* <main className="bg-gray-500 rounded-2xl bg-opacity-60 h-[140px] flex justify-between md:justify-around items-center px-4">
-        <div className=" h-full py-4">
-          <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
-          <h1 className=" text-2xl font-extrabold ">میکس پلیتر</h1>
+
+      {/* *******-slider-***** */}
+
+      <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
+        <div className=" h-full py-4 pl-4">
+          <p className=" mb-6 text-gray-300 text-lg ">{currentItem.text}</p>
+          <h1 className=" text-2xl font-extrabold ">{currentItem.title}</h1>
           <div className=" flex gap-5 text-sm">
-            <p className="text-gray-200">مخصوص</p>
-            <p className="text-gray-200">زمان: 30 دقیقه</p>
+            <p className="text-gray-200">{currentItem.text_l}</p>
+            <p className="text-gray-200">{currentItem.text_r}</p>
           </div>
         </div>
-        <div>
-          <img src="/images/1.png" alt="" />
+        <div className=" pr-4">
+          <img
+            key={currentItem.id}
+            src={currentItem.img}
+            alt={"loading..."}
+            className="w-[120px] object-cover"
+          />
         </div>
-      </main> */}
+
+        {/* bouttons */}
+        <button
+          onClick={prevSlide}
+          className="absolute left-0 bg-stone-700 text-white p-2 rounded-full"
+        >
+          &lt;
+        </button>
+        <button
+          onClick={nextSlide}
+          className="absolute  right-0 bg-stone-700 text-white p-2 rounded-full"
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/data/slide.js
@@ -0,0 +1,26 @@
+export const slides = [
+  {
+    id: 1,
+    text: "پیشنهاد ارگ",
+    title: "پلیتر",
+    text_l: "مخصوص",
+    text_r: "زمان: 30 دقیقه",
+    img: "/images/1.png",
+  },
+  {
+    id: 1,
+    text: "پیشنهاد ارگ",
+    title: "پیتزا",
+    text_l: "مخصوص",
+    text_r: "زمان: 30 دقیقه",
+    img: "/images/3.png",
+  },
+  {
+    id: 1,
+    text: "پیشنهاد ارگ",
+    title: "اکبر جوجه",
+    text_l: "مخصوص",
+    text_r: "زمان: 50 دقیقه",
+    img: "/images/2.png",
+  },
+];

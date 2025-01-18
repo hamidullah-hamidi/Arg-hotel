3c92a72d8a1ea6d762768b9c8082b97491d3f167
Hamidullah <hamidullahh754@gmail.com>
2025-01-19 00:43:39 +0430

    u

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -14,43 +14,43 @@ function Slide() {
 
   return (
     <Slider {...settings}>
-      <div className="Slider relative bg-gray-500 rounded-2xl bg-opacity-60  flex justify-between md:justify-around items-center px-4  ">
-        <div className=" h-full py-4 pl-4">
+      <div className=" text-right relative bg-gray-500 rounded-2xl h-[140px] bg-opacity-60  flex justify-between md:justify-around items-center px-8  ">
+        <div className=" absolute top-0 left-4 w-[150px] ">
+          <img src="/images/3.png" alt={'loading...'} />
+        </div>
+        <div className=" relative py-4 pl-4">
           <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
           <h1 className=" text-2xl font-extrabold ">پیتزا</h1>
-          <div className=" flex gap-5 text-sm">
+          <div className=" flex gap-5 text-sm absolute right-0">
             <p className="text-gray-200">مخصوص</p>
             <p className="text-gray-200">زمان: 30 دقیقه</p>
           </div>
         </div>
-        <div className=" absolute top-0 right-0 w-[150px] pr-4">
-          <img src="/images/3.png" alt={'loading...'} />
-        </div>
       </div>
-      <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
-        <div className=" h-full py-4 pl-4">
-          <p className=" mb-6 text-gray-200 text-lg font-bold ">پیشنهاد ارگ</p>
+      <div className=" text-right relative bg-gray-500 rounded-2xl h-[140px] bg-opacity-60  flex justify-between md:justify-around items-center px-8  ">
+        <div className=" absolute -top-1 left-4 w-[150px] ">
+          <img src="/images/1.png" alt={'loading...'} />
+        </div>
+        <div className=" relative py-4 pl-4">
+          <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
           <h1 className=" text-2xl font-extrabold ">پلیتر</h1>
-          <div className=" flex gap-5 text-sm">
+          <div className=" flex gap-5 text-sm absolute right-0 ">
             <p className="text-gray-200">مخصوص</p>
             <p className="text-gray-200">زمان: 45 دقیقه</p>
           </div>
         </div>
-        <div className="absolute top-0 right-0 w-[150px] pr-4">
-          <img src="/images/1.png" alt={'loading...'} />
-        </div>
       </div>
-      <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
-        <div className=" h-full py-4 pl-4">
+      <div className=" text-right relative bg-gray-500 rounded-2xl h-[140px] bg-opacity-60  flex justify-between md:justify-around items-center px-8  ">
+        <div className=" absolute -top-1 left-4 w-[150px] ">
+          <img src="/images/2.png" alt={'loading...'} />
+        </div>
+        <div className=" relative py-4 pl-4">
           <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
           <h1 className=" text-2xl font-extrabold ">کباب</h1>
-          <div className=" flex gap-5 text-sm">
+          <div className=" flex gap-5 text-sm absolute right-0">
             <p className="text-gray-200">مخصوص</p>
             <p className="text-gray-200">زمان: 50 دقیقه</p>
           </div>
-        </div>
-        <div className="absolute top-0 right-0 w-[150px] pr-4">
-          <img src="/images/2.png" alt={'loading...'} />
         </div>
       </div>
     </Slider>

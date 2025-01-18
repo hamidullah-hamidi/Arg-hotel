fd25e64fcefd0a6923b5b5959cdfa5c86eac9912
Hamidullah <hamidullahh754@gmail.com>
2025-01-19 00:43:39 +0430

    u

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,11 +1,11 @@
-import { foods } from "../data/foods";
+import { foods } from '../data/foods';
 
 const foodStyle =
-  "w-36 h-44 mt-6 bg-gray-500 bg-opacity-40 flex flex-col justify-center items-center rounded-3xl cursor-pointer hover:shadow-2xl hover:bg-opacity-50";
+  'w-36 h-44 mt-6 bg-gray-500 bg-opacity-40 flex flex-col justify-center items-center rounded-3xl cursor-pointer hover:shadow-2xl hover:bg-opacity-50';
 
 function Food() {
   return (
-    <ul className="second mb-0 pt-6 w-full h-full flex flex-wrap justify-center items-center gap-4 md:gap-8 overflow-y-scroll">
+    <ul className="second xl:-mt-20 mt-3 mb-0 pt-6 w-full h-full overflow-y-scroll flex flex-wrap justify-center items-center gap-4 md:gap-8">
       {foods.map((food) => (
         <li className={foodStyle} key={food.id}>
           <img src={food.img} alt="" />

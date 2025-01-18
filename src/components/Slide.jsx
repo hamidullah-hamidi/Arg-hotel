618eda311669dafa8242af39a6ca3b341bcac12f
Hamidullah <hamidullahh754@gmail.com>
2025-01-19 00:07:26 +0430

    u

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,17 +1,20 @@
 import Slider from 'react-slick';
 
 function Slide() {
-  var settings = {
+  let settings = {
     dots: true,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: false,
   };
 
   return (
     <Slider {...settings}>
-      <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
+      <div className="Slider relative bg-gray-500 rounded-2xl bg-opacity-60  flex justify-between md:justify-around items-center px-4  ">
         <div className=" h-full py-4 pl-4">
           <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
           <h1 className=" text-2xl font-extrabold ">پیتزا</h1>
@@ -20,12 +23,8 @@ function Slide() {
             <p className="text-gray-200">زمان: 30 دقیقه</p>
           </div>
         </div>
-        <div className=" pr-4">
-          <img
-            src="/images/3.png"
-            alt={'loading...'}
-            className="w-[120px] object-cover"
-          />
+        <div className=" absolute top-4 right-0 w-[120px] pr-4">
+          <img src="/images/3.png" alt={'loading...'} />
         </div>
       </div>
       <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
@@ -37,12 +36,8 @@ function Slide() {
             <p className="text-gray-200">زمان: 45 دقیقه</p>
           </div>
         </div>
-        <div className=" pr-4">
-          <img
-            src="/images/1.png"
-            alt={'loading...'}
-            className="w-[120px] object-cover"
-          />
+        <div className="absolute top-4 right-0 w-[120px] pr-4">
+          <img src="/images/1.png" alt={'loading...'} />
         </div>
       </div>
       <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
@@ -54,12 +49,8 @@ function Slide() {
             <p className="text-gray-200">زمان: 50 دقیقه</p>
           </div>
         </div>
-        <div className=" pr-4">
-          <img
-            src="/images/2.png"
-            alt={'loading...'}
-            className="w-[120px] object-cover"
-          />
+        <div className="absolute top-4 right-0 w-[120px] pr-4">
+          <img src="/images/2.png" alt={'loading...'} />
         </div>
       </div>
     </Slider>

be079a89c4040e9da63322f2d2fe76193d71f28a
Hamidullah <hamidullahh754@gmail.com>
2025-01-19 00:14:06 +0430

    u

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -4,7 +4,7 @@ function Slide() {
   let settings = {
     dots: true,
     infinite: true,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000,
     speed: 500,
     slidesToShow: 1,
@@ -23,20 +23,20 @@ function Slide() {
             <p className="text-gray-200">زمان: 30 دقیقه</p>
           </div>
         </div>
-        <div className=" absolute top-4 right-0 w-[120px] pr-4">
+        <div className=" absolute top-0 right-0 w-[150px] pr-4">
           <img src="/images/3.png" alt={'loading...'} />
         </div>
       </div>
       <div className="relative bg-gray-500 rounded-2xl bg-opacity-60 h-[140px]  flex justify-between md:justify-around items-center px-4">
         <div className=" h-full py-4 pl-4">
-          <p className=" mb-6 text-gray-300 text-lg ">پیشنهاد ارگ</p>
+          <p className=" mb-6 text-gray-200 text-lg font-bold ">پیشنهاد ارگ</p>
           <h1 className=" text-2xl font-extrabold ">پلیتر</h1>
           <div className=" flex gap-5 text-sm">
             <p className="text-gray-200">مخصوص</p>
             <p className="text-gray-200">زمان: 45 دقیقه</p>
           </div>
         </div>
-        <div className="absolute top-4 right-0 w-[120px] pr-4">
+        <div className="absolute top-0 right-0 w-[150px] pr-4">
           <img src="/images/1.png" alt={'loading...'} />
         </div>
       </div>
@@ -49,7 +49,7 @@ function Slide() {
             <p className="text-gray-200">زمان: 50 دقیقه</p>
           </div>
         </div>
-        <div className="absolute top-4 right-0 w-[120px] pr-4">
+        <div className="absolute top-0 right-0 w-[150px] pr-4">
           <img src="/images/2.png" alt={'loading...'} />
         </div>
       </div>

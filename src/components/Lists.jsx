488bc240d9d7b564dbf1c313dcd4d5593ed12b9f
Hamidullah <hamidullahh754@gmail.com>
2025-01-19 22:28:34 +0430

    u

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -5,7 +5,7 @@ function Lists() {
     'flex w-fit py-1 px-3 rounded-lg bg-gray-500 bg-opacity-60 gap-2 justify-between items-center text-lg';
 
   return (
-    <div className="lists px-8 h-screen">
+    <div className="lists px-8 h-screen flex flex-col">
       <h1 className="text-2xl my-4 text-right">دسته بندی ها</h1>
 
       <ul className="first flex gap-2 justify-center items-center ">

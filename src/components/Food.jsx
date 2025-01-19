3244ef27d7e957aec8d640e4b6079ec7b11eee6c
Hamidullah <hamidullahh754@gmail.com>
2025-01-20 01:10:48 +0430

    completed page 3

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { foods } from '../data/foods';
 
 const foodStyle =
@@ -7,12 +8,14 @@ function Food() {
   return (
     <ul className="second mt-3 py-6 w-full h-full overflow-y-auto flex flex-wrap justify-center items-center gap-8 md:gap-8">
       {foods.map((food) => (
-        <li className={foodStyle} key={food.id}>
-          <img src={food.img} alt="" />
-          <h1 className=" text-2xl my-2">{food.title}</h1>
-          <p className=" text-yellow-500">{food.star}</p>
-          <p className="text-sm">{food.time}</p>
-        </li>
+        <Link to="/menu/food" key={food.id}>
+          <li className={foodStyle} key={food.id}>
+            <img src={food.img} alt="" />
+            <h1 className=" text-2xl my-2">{food.title}</h1>
+            <p className=" text-yellow-500">{food.star}</p>
+            <p className="text-sm">{food.time}</p>
+          </li>
+        </Link>
       ))}
     </ul>
   );

6f51b23838dc12db3d4f773023951777bd78bdbf
Hamidullah <hamidullahh754@gmail.com>
2025-01-22 22:19:42 +0430

    u

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import { foods } from '../data/foods';
 
-  // در اینج از استیت استفاده کردم بخاطر انتقال اطلاعات 
-  // state 
-
+// در اینج از استیت استفاده کردم بخاطر انتقال اطلاعات
 
 const foodStyle =
   'w-36 h-44 mt-8 bg-gray-500 bg-opacity-40 flex flex-col justify-center items-center rounded-3xl cursor-pointer hover:shadow-2xl hover:bg-opacity-50';
 
-function Food() {
+function Food({ selecetedCatagory }) {
+  const filteredFoods = foods.filter((food) =>
+    selecetedCatagory ? food.catagory === selecetedCatagory : true,
+  );
   return (
     <ul className="second mt-3 py-6 w-full h-full overflow-y-auto flex flex-wrap justify-center items-center gap-8 md:gap-8">
-      {foods.map((food) => (
+      {filteredFoods.map((food) => (
         <Link to="/menu/food" key={food.id} state={{ food }}>
           <li className={foodStyle} key={food.id}>
             <img src={food.img} alt="" />
--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react';
 import Food from './Food';
 
 function Lists() {
-  const [selecetedCatagory, setSelectedCatagory] = useState('');
+  const [selecetedCatagory, setSelectedCatagory] = useState('pizza');
+
+  function handleClick(catagory) {
+    setSelectedCatagory(catagory);
+    console.log('clicked');
+  }
 
   const list =
     'flex w-fit py-1 px-3 rounded-lg bg-gray-500 bg-opacity-60 gap-2 justify-center items-center text-lg cursor-pointer active:scale-75 transition-all duration-500 ';
@@ -11,25 +16,25 @@ function Lists() {
     <div className="lists px-8 h-screen flex flex-col ">
       <h1 className="text-2xl my-4 text-right">دسته بندی ها</h1>
       <ul className="first flex gap-2 justify-center items-center  ">
-        <li className={list}>
+        <li className={list} onClick={() => handleClick('coffee')}>
           <img src="/images/1.png" alt="" />
-          <p>آش</p>
-        </li>
-        <li className={list}>
-          <img src="/images/2.png" alt="" />
-          <p>کباب</p>
-        </li>
-        <li className={list}>
-          <img src="/images/3.png" alt="" />
           <p>قهوه</p>
         </li>
-        <li className={list}>
+        <li className={list} onClick={() => handleClick('spaghetii')}>
+          <img src="/images/2.png" alt="" />
+          <p>ماکارونی</p>
+        </li>
+        <li className={list} onClick={() => handleClick('steak')}>
+          <img src="/images/3.png" alt="" />
+          <p>کباب</p>
+        </li>
+        <li className={list} onClick={() => handleClick('pizza')}>
           <img src="/images/1.png" alt="" />
           <p>پیتزا</p>
         </li>
       </ul>
       <div className="overflow-hidden">
-        <Food />
+        <Food selecetedCatagory={selecetedCatagory} />
       </div>
     </div>
   );

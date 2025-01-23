b5281f5a7819d4fd94248e6f6043e80262eb6de5
Hamidullah <hamidullahh754@gmail.com>
2025-01-23 21:36:47 +0430

    u

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -10,37 +10,43 @@ function Lists() {
   }
 
   const list =
-    'flex w-fit py-1 px-3 rounded-lg bg-gray-500 bg-opacity-60 gap-2 justify-center items-center text-lg cursor-pointer active:scale-75 transition-all duration-500 ';
-
-  const active = 'bg-red-900';
+    'flex w-fit py-1 px-3 rounded-lg  bg-opacity-60 gap-2 justify-center items-center text-lg cursor-pointer active:scale-75 transition-all duration-500 ';
 
   return (
     <div className="lists px-8 h-screen flex flex-col slide-down ">
       <h1 className="text-2xl my-4 text-right">دسته بندی ها</h1>
       <ul className="first flex gap-2 justify-center items-center  ">
         <li
-          className={`${list} ${selecetedCatagory === 'coffee' && active}`}
+          className={`${list} ${
+            selecetedCatagory === 'coffee' ? 'bg-red-900' : 'bg-gray-500'
+          }`}
           onClick={() => handleClick('coffee')}
         >
           <img src="/images/1.png" alt="" />
           <p>قهوه</p>
         </li>
         <li
-          className={`${list} ${selecetedCatagory === 'spaghetii' && active}`}
+          className={`${list} ${
+            selecetedCatagory === 'spaghetii' ? 'bg-red-900' : 'bg-gray-500'
+          }`}
           onClick={() => handleClick('spaghetii')}
         >
           <img src="/images/2.png" alt="" />
           <p>ماکارونی</p>
         </li>
         <li
-          className={`${list} ${selecetedCatagory === 'steak' && active}`}
+          className={`${list} ${
+            selecetedCatagory === 'steak' ? 'bg-red-900' : 'bg-gray-500'
+          }`}
           onClick={() => handleClick('steak')}
         >
           <img src="/images/3.png" alt="" />
           <p>کباب</p>
         </li>
         <li
-          className={`${list} ${selecetedCatagory === 'pizza' && active}`}
+          className={`${list} ${
+            selecetedCatagory === 'pizza' ? 'bg-red-900' : 'bg-gray-500'
+          }`}
           onClick={() => handleClick('pizza')}
         >
           <img src="/images/1.png" alt="" />
